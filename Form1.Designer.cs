9261894ea45e1e11e1effcfd9d154d76ae4a7fa1
Steph Bachabi <stephbachabi@outlook.com>
2024-09-06 06:47:36 -0700

    UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            FunctionOpener = new Button();
+            SampleViewer = new Button();
+            YAxis = new Panel();
+            XAxis = new Panel();
+            SuspendLayout();
+            // 
+            // FunctionOpener
+            // 
+            FunctionOpener.AutoSize = true;
+            FunctionOpener.BackColor = Color.DimGray;
+            FunctionOpener.Font = new Font("Gabriola", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FunctionOpener.ForeColor = Color.Red;
+            FunctionOpener.Location = new Point(12, 12);
+            FunctionOpener.Name = "FunctionOpener";
+            FunctionOpener.Size = new Size(100, 55);
+            FunctionOpener.TabIndex = 1;
+            FunctionOpener.Text = "Function";
+            FunctionOpener.UseVisualStyleBackColor = false;
+            FunctionOpener.Click += FunctionOpener_Click;
+            // 
+            // SampleViewer
+            // 
+            SampleViewer.AutoSize = true;
+            SampleViewer.BackColor = Color.DimGray;
+            SampleViewer.Font = new Font("Gabriola", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            SampleViewer.ForeColor = Color.Red;
+            SampleViewer.Location = new Point(12, 66);
+            SampleViewer.Name = "SampleViewer";
+            SampleViewer.Size = new Size(100, 55);
+            SampleViewer.TabIndex = 2;
+            SampleViewer.Text = "Sample";
+            SampleViewer.UseVisualStyleBackColor = false;
+            SampleViewer.Click += SampleViewer_Click;
+            // 
+            // YAxis
+            // 
+            YAxis.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            YAxis.BackColor = SystemColors.ControlDarkDark;
+            YAxis.Location = new Point(408, 0);
+            YAxis.Name = "YAxis";
+            YAxis.Size = new Size(1, 487);
+            YAxis.TabIndex = 3;
+            // 
+            // XAxis
+            // 
+            XAxis.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            XAxis.BackColor = SystemColors.ControlDarkDark;
+            XAxis.Location = new Point(0, 244);
+            XAxis.Name = "XAxis";
+            XAxis.Size = new Size(800, 1);
+            XAxis.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(XAxis);
+            Controls.Add(YAxis);
+            Controls.Add(SampleViewer);
+            Controls.Add(FunctionOpener);
+            Name = "Form1";
+            Text = "Extended Project Qualification";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button FunctionOpener;
+        private Button SampleViewer;
+        private Panel YAxis;
+        private Panel XAxis;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,125 @@
 namespace EPQ1
 {
+    using System.Diagnostics;
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        List<Char> AllowedKeys = new List<Char>()
+        {  
+           '1',
+           '2',
+           '3',
+           '4',
+           '5',
+           '6',
+           '7',
+           '8',
+           '9',
+           '0',
+           '(',
+           ')',
+           '-',
+           '=',
+           '+',
+           '*',
+           '^',
+           '!',
+           'x'
+        };
+        bool EquationOpen = false;
+        bool SampleOpen = false;
+        string EquationText = "3x^(2)";
+        string SampleNum = "50";
+        public TextBox FunctionEquation { get; private set; }
+        public TextBox SampleNumber { get; private set; }
+        private void FunctionOpener_Click(object sender, EventArgs e)
+        {
+            if (!EquationOpen)
+            {
+                TextBox frame = new TextBox();
+                frame.Name = "FunctionEquation";
+                frame.Location = new System.Drawing.Point(100, 0);
+                frame.Size = new System.Drawing.Size(500, 100);
+                frame.TextChanged += Equation_TextChanged;
+                frame.Text = EquationText;
+                this.Controls.Add(frame);
+                this.FunctionEquation = frame;
+                frame.BringToFront();
+                EquationOpen = true;
+            }
+            else
+            {
+                EquationText = this.FunctionEquation.Text;
+                EquationOpen = false;
+                this.FunctionEquation.TextChanged -= Equation_TextChanged;
+                this.Controls.Remove(this.FunctionEquation);
+                
+            }
+        }
+       
+        private void Equation_TextChanged(object sender, EventArgs e)
+        {
+            if (!EquationOpen) { return; }
+            char[] textArray = this.FunctionEquation.Text.ToCharArray();
+            int length = this.FunctionEquation.TextLength;
+            int i = 0;
+            foreach (char v in textArray)
+            {
+                i += 1;
+                if (AllowedKeys.IndexOf(v) == -1)
+                {
+                    this.FunctionEquation.Text = this.FunctionEquation.Text.Substring(0, i - 1) + this.FunctionEquation.Text.Substring(i);
+                }
+            }
+        }
+        private void SampleViewer_Click(object sender, EventArgs e)
+        {
+            if (!SampleOpen)
+            {
+                TextBox frame = new TextBox();
+                frame.Name = "SampleNumber";
+                frame.Location = new System.Drawing.Point(100, 100);
+                frame.Size = new System.Drawing.Size(100, 100);
+                frame.Text = SampleNum;
+                this.Controls.Add(frame);
+                frame.TextChanged += Sample_TextChanged;
+                this.Controls.Add(frame);
+                this.SampleNumber = frame;
+                frame.BringToFront();
+                SampleOpen = true;
+            }
+            else
+            {
+                SampleNum = this.SampleNumber.Text;
+                SampleOpen = false;
+                this.SampleNumber.TextChanged -= Sample_TextChanged;
+                this.Controls.Remove(this.SampleNumber);
+
+            }
+        }
+        private void Sample_TextChanged(object sender, EventArgs e)
+        {
+            if (!SampleOpen) { return; }
+            char[] textArray = this.SampleNumber.Text.ToCharArray();
+            int length = this.SampleNumber.TextLength;
+            int i = 0;
+            foreach (char v in textArray)
+            {
+                i += 1;
+                if (!Char.IsNumber(v))
+                {
+                    this.SampleNumber.Text = this.SampleNumber.Text.Substring(0, i - 1) + this.SampleNumber.Text.Substring(i);
+                }
+            }
         }
     }
 }
